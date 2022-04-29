202cbf51a6ba94a43389e2df0c96c1658055e0a6
Venky Ramasubramanian <venky.ramasub@gmail.com>
2022-04-28 21:38:42 -0700

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "instance1" {
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
-    Name = "instance3"
+    Name = "instance4"
   }
 }
 output "instance_public_ip" {
