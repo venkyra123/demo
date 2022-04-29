33cea5615fc6fb0fff9c148344401d49ff7bde0c
Venky Ramasubramanian <venky.ramasub@gmail.com>
2022-04-28 20:05:32 -0700

    Create first_ec2.tf

---
+++ first_ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance1" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "instance3"
+  }
+}
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.instance1.public_ip
+}
