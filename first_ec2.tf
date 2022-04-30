e2ad0acb3ab8d3e034dfd29ad10a841803175dc4
Venky Ramasubramanian <venky.ramasub@gmail.com>
2022-04-29 21:14:26 -0700

    Create first_ec2.tf

---
+++ first_ec2.tf
@@ -0,0 +1,17 @@
+variable "ami" {}
+variable "region" {}
+variable "instance_type" {}
+variable "access_key" {}
+variable "secret_key" {}
+resource "aws_instance" "instance1" {
+ami  = var.ami
+instance_type = var.instance_type
+tags = {
+Name = "instance4"
+}
+}
+
+output "instance_public_ip" {
+description = "Public IP address of the EC2 instance"
+value  = aws_instance.instance1.public_ip
+}
